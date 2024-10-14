ce7e4da2310fdf05da85d7ee6974c29330107e66
Bruno Borjes <borjesbruno96@icloud.com>
2024-10-14 20:52:17 -0300

    Menu Error Created

---
+++ src/Components/Menu/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './style';
+
+const Menu = () => {
+    return (
+        <Container>
+            <h1>Menu</h1>
+        </Container>
+    );
+}
+
+export default Menu;
---
+++ src/Components/Menu/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div` 
+    width: 100%;
+    max-width: 1200px;
+    background-color: white;
+    height: 50px;//altura
+    position: fixed;
+    top: 0;
+    margin: 0 auto;
+`
